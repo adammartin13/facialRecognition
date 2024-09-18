66fa1cb92dba38f812c9cacfc90dc4f8a04394b3
adammartin13 <christopheradammartin13@gmail.com>
2024-09-18 13:28:21 -0500

    Delete face_request.py

--- face_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-# This code doesn't work, need to adjust the request string
-api_key = "jQKW91WJG8YuxJETOFn51w"
-r = requests.get('https://api.generated.photos/api/v1/faces?api_key='+api_key+'&faces?per_page=1')
-output = json.loads(r.json())
-print(output)
-print(r)
